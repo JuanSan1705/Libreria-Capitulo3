1a3c9c2cb696832c2bafd8d46ade1b85e79cc6d0
JuanSan1705 <60147407+JuanSan1705@users.noreply.github.com>
2020-03-11 17:36:06 -0500

    Update LibCapTres.py

--- LibCapTres.py
+++ LibCapTres.py
@@ -1,53 +1,91 @@
-def Canicas(m, x, click, cont):
-    if len(m[0]) != len(x):
-        return "No se puede realizar la operación"
+import matplotlib.pyplot as plt
+import Operaciones as Op
+import math
+import numpy as np
+
+def Canicas(Matriz, X, Click, cont):
+    for i in range (len(X)):
+        for j in range(len(X)):
+            if Matriz[i][j] != 1:
+                if Matriz[i][j] != 0:
+                    return print('Solo valores booleanos')
+
+    if len(Matriz[0]) != len(X):
+        return "No se puede realizar la operacion"
     else:
         mat = []
-        for i in range(len(m)):
+        for i in range(len(Matriz)):
             Sum = 0
-            for j in range(len(m)):
-                mult = x[j] * m[i][j]
+            for j in range(len(Matriz)):
+                mult = X[j] * Matriz[i][j]
                 Sum = Sum + mult
             mat.append(Sum)
-        if click == cont:
-            return mat
-        else:
-            x = mat
-            cont += 1
-            Canicas(m, x, click, cont)
-
-def Cambio(m,x,NumRend,cont):
-    n = m
-    DobleRendija(m, x, NumRend, cont, n)
-
-def DobleRendija(m, x, NumRend, cont, n):
-    matriz = []
-    if NumRend == 1:
-        com = []
-        for i in range(len(m)):
-            Sum = 0
-            for j in range(len(m)):
-                mult = x[j] * m[i][j]
-                Sum = Sum + mult
-            com.append(Sum)
-        return print(com)
-    else:
-        if NumRend == cont:
-            mat = []
-            for i in range(len(m)):
-                Sum = 0
-                for j in range(len(m)):
-                    mult = x[j] * m[i][j]
-                    Sum = Sum + mult
-                mat.append(Sum)
+        if Click == cont:
             return print(mat)
         else:
+            X = mat
             cont += 1
-            for h in range(len(m)):
-                matriz.append([0] * len(m))
-            for i in range(len(m)):
-                for j in range(len(m[0])):
-                    for k in range(len(m)):
-                        matriz[i][j] += m[i][k] * n[k][j]
-            m = matriz
-            DobleRendija(m, x, NumRend, cont, n)
+            Canicas(Matriz, X, Click, cont)
+
+def RendijaProbabilistico(Matriz, X, Click, cont):
+    sum = 0
+    sum2 = 0
+    for i in range(len(Matriz)):
+        for j in range(len(X)):
+            sum += Matriz[i][j]
+            sum2 += Matriz[j][i]
+    if sum == len(Matriz) and sum2 == len(Matriz):
+        if len(Matriz[0]) != len(X):
+            return "No se puede realizar la operacion"
+        else:
+            mat = []
+            for i in range(len(Matriz)):
+                Sum = 0
+                for j in range(len(Matriz)):
+                    mult = X[j] * Matriz[i][j]
+                    Sum = Sum + mult
+                mat.append(Sum)
+            if Click == cont:
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                xx = range(len(mat))
+                ax.bar(xx, mat)
+                plt.xlabel('Estado')
+                plt.ylabel('Porcetaje')
+                plt.title('EvoluciÃ³n dinÃ¡mica del sistema despues de '+ str(Click) + ' Clicks de tiempo')
+                plt.show()
+                return mat
+            else:
+                X = mat
+                cont += 1
+                RendijaProbabilistico(Matriz, X, Click, cont)
+
+
+
+def Cambio(Matriz, X, Click, cont):
+     n = Matriz
+     DobleRendija(Matriz, X, Click, cont, n)
+
+def DobleRendija(Matriz, X, Click, cont, n):
+    if cont == Click:
+        S = Op.multiplicacionMatrizVector(n, X)
+        Na = []
+        for j in range(len(S)):
+            Na.append(S[j][0])
+        MATRIX = []
+        for i in range(len(Na)):
+            A = Op.modulo(Na[i])
+            MATRIX.append(A)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        xx = range(len(MATRIX))
+        ax.bar(xx, MATRIX)
+        plt.xlabel('Estado')
+        plt.ylabel('Porcetaje')
+        plt.title('Evolucion dinamica del sistema despues de '+ str(Click) + ' Clicks de tiempo')
+        plt.show()
+        return 
+    else:
+        n = Op.multiplicacionMatrizMatriz(n, Matriz)
+        cont += 1
+        DobleRendija(Matriz, X, Click, cont, n)
